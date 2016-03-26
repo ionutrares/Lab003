f1bf11595d0066b29442e68c41ae4de93efae420
ionutraresbucur2@gmail.com <ionutraresbucur2@gmail.com>
2016-03-26 13:51:35 +0200

    222

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Salut Lume!");
-        System.out.println("Salut Lume2!");
+        System.out.println("Salut Lume222!");
     }
 }
